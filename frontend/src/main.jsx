32d681c167beac44e0facb3bf391fe7523faac3a
unknown <domahlawrance9@gmail.com>
2025-11-21 01:24:14 +0200

    Rename FeedbackForm.css to feedbackForm.css

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import './styles/feedbackForm.css';
 import App from './App';  // import App, not FeedbackForm
 
 createRoot(document.getElementById('root')).render(
